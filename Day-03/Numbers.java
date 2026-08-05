9fe84cb82a07629489867a41bcee152215ba4f9e
Pranaya Bubanale <pranayabubanale8@gmail.com>
2026-08-05 23:48:15 +0530

    Add files via upload

---
+++ Day-03/Numbers.java
@@ -0,0 +1,8 @@
+public class Numbers {
+    public static void main(String[] args){
+        for(int i=0; i<11; i++){
+            System.out.print(i+" ");
+        }
+    }
+    
+}
---
+++ Day-03/exmple.java
@@ -0,0 +1,15 @@
+public class exmple {
+    public static void main(String[] args) {
+
+        int i =12;
+        while(i<11){
+            System.out.println(i);
+        }
+        do{
+            System.out.println(i);
+        }while(i<11);
+    }
+
+        }
+
+    
